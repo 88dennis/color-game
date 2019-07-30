f466bd0a37f116116044bc99ce9320ab089bc7b9
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2019-07-30 17:49:00 +0800

    up

--- colorGame.js
+++ colorGame.js
@@ -8,8 +8,32 @@ let colors = [
 ]
 
 let boxes = document.querySelectorAll(".boxes");
+let guessThisColor = colors[3];
+let colorDisplay = document.querySelector("#colorDisplay")
+let messageDisplay = document.querySelector("#message")
 
+colorDisplay.textContent = guessThisColor;
 
 for (let i = 0; i < boxes.length; i++) {
-    boxes[i].style.backgroundColor = colors[i]
+    //add colors to boxes
+    boxes[i].style.backgroundColor = colors[i];
+    //add click listeners to boxes
+    boxes[i].addEventListener("click", function() {
+        //get color of clicked box
+        var clickedColor = this.style.backgroundColor
+
+        if (clickedColor === guessThisColor) {
+            messageDisplay.textContent = "Correct"
+            changeAllColors(clickedColor)
+        } else {
+            this.style.backgroundColor = "black";
+            messageDisplay.textContent = "Try Again"
+        }
+    })
+}
+
+function changeAllColors(color) {
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].style.backgroundColor = color
+    }
 }
